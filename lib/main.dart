57fe1a23c670fbe873e4c5719367bc83f953a311
meRakibul <hralibul@gmail.com>
2024-08-20 00:32:16 +0600

    Log in Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,144 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main(){
   runApp(const Myapp());
 }
-
-class Myapp extends StatelessWidget {
+class Myapp extends StatelessWidget{
   const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeActivity(),
+    return MaterialApp(home: HomeActivity(),
     debugShowCheckedModeBanner: false,);
   }
-
+  
 }
-
-class HomeActivity extends StatelessWidget {
+class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
-  Mysnack(message,context){
+  Mysnack(message, context){
     return ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text(message))
     );
   }
 
- MyallertDialog(context){
+
+  
+  MyallertDialog(context){
     return showDialog(
         context: context,
         builder: (BuildContext context){
           return Expanded(child: AlertDialog(
-            title: Text("Allert!"),
-            content: Text("Do You Want To Access Gallery"),
-            actions: [
-              TextButton(onPressed: (){
-                Navigator.of(context).pop();
-                Mysnack("Accepted", context);}, child: Text("Yes")),
-              TextButton(onPressed: (){
-                Mysnack("Denied", context);
-                Navigator.of(context).pop();}, child: Text("No")),
-            ],
-          ));
-          
+            title:Text("Warning"),
+            content: Text("Do You Want TO Continue?"),
+          actions: [
+            TextButton(onPressed: (){
+              Mysnack("Accepted", context);
+              Navigator.of(context).pop();},
+                
+                child: Text("Yes")),
+            TextButton(onPressed: (){
+              Mysnack("Denied", context);
+              Navigator.of(context).pop();},
+
+                child: Text("No"))
+          ],));
         }
     );
- }
+  }
 
   @override
   Widget build(BuildContext context) {
     ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-        padding:EdgeInsets.all(25),
-      backgroundColor: Colors.cyanAccent,
-      foregroundColor: Colors.green,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.elliptical(30, 20))
-      )
+        minimumSize: Size(double.infinity, 60),
+      backgroundColor: Colors.purpleAccent,
+      foregroundColor: Colors.tealAccent
     );
-
-
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dialar Demo App"),
-        backgroundColor: Colors.greenAccent,
+      title: Text("Dattapra Dialing"),
+        backgroundColor: Colors.pink,
         actions: [
-          IconButton(onPressed: (){Mysnack("This is for Home", context);}, icon: Icon(Icons.home)),
-          IconButton(onPressed: (){Mysnack("This is time Icon", context);}, icon: Icon(Icons.watch_later)),
-          IconButton(onPressed: (){Mysnack("Account", context);}, icon: Icon(Icons.account_circle)),
+          IconButton(onPressed: (){MyallertDialog(context);
+            Mysnack("Accepted", context);}, icon: Icon(Icons.account_circle)),
+          IconButton(onPressed: (){MyallertDialog(context);
+          Mysnack("Accepted", context);}, icon: Icon(Icons.search_sharp)),
+          IconButton(onPressed: (){MyallertDialog(context);
+          Mysnack("Accepted", context);}, icon: Icon(Icons.add)),
         ],
+        
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: (){Mysnack("Dialing", context);},
-      backgroundColor: Colors.teal,
-      child: Icon(Icons.dialpad),), 
-      bottomNavigationBar: BottomNavigationBar(
-       currentIndex: 1,
-    items: [
-          BottomNavigationBarItem(icon: Icon(Icons.add_call), label: "Call"),
-         BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
-    BottomNavigationBarItem(icon: Icon(Icons.contact_mail_outlined),label: "Contact"),
-        ],
-        onTap: (int index){
-         if (index == 0){
-           Mysnack("Make a Call", context);
-         }
-         if (index ==1){
-           Mysnack("Home", context);
-         }
-         if(index==2){
-           Mysnack("Contact", context);
-         }
-        },),
+          onPressed: (){MyallertDialog(context);},
+      backgroundColor: Colors.tealAccent,
+      child: Icon(Icons.dialpad),),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon:Icon(Icons.home),label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.settings),label: "Settings"),
+        BottomNavigationBarItem(icon: Icon(Icons.account_balance),label: "Balance")
+      ],
+      onTap:
+      (int index){
+        if (index==0){
+          MyallertDialog(context);
+        }
+        if(index==1){
+          MyallertDialog(context);
+        }
+        if(index==1){
+          Mysnack("Insufficient Balance", context);
+        }
+      },),
       drawer: Drawer(
-   child: ListView(
-     children: [
-       DrawerHeader(
-         padding: EdgeInsets.all(0),
-         child:
-       UserAccountsDrawerHeader(
-         accountName: Text("Rakib"), 
-         accountEmail: Text("rakib@insfo.com"),
-       currentAccountPicture: Image.network("https://i.postimg.cc/Y0yTmYrP/IMG-3417.jpg"),
-       decoration: BoxDecoration(color: Colors.blueGrey),),
-       ),
-       ListTile(leading: Icon(Icons.home), title: Text("Home"), onTap:(){Mysnack("This is Home", context);}),
-       ListTile(
-           leading: Icon(Icons.man),
-           title: Text("Profile"),
-           onTap: (){Mysnack("Check your Profile", context);}),
-       ListTile(
-           leading: Icon(Icons.contact_mail_outlined),
-           title: Text("Contact"),
-           onTap: (){Mysnack("Check your Profile", context);}),
-       ListTile(
-           leading:Icon(Icons.account_balance_wallet_outlined),
-           title: Text("About"),
-           onTap: (){
-             Mysnack("Check your Profile", context);
-           }
-       ),
-     ],
-   ),
- ),
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+                child:
+                UserAccountsDrawerHeader(
+                    accountName: Text("Rakibul Hasan"),
+                    accountEmail: Text("rakib@info.com"),
+                currentAccountPicture: Image.network("https://i.postimg.cc/Y0yTmYrP/IMG-3417.jpg"),
+                decoration: BoxDecoration(color: Colors.purpleAccent),),),
+               ListTile(leading: Icon(Icons.home),title: Text("Home"),onTap: (){MyallertDialog(context);},),
+              ListTile(leading: Icon(Icons.settings),title: Text("Setting"),onTap: (){MyallertDialog(context);},),
+              ListTile(leading: Icon(Icons.accessibility_sharp),title: Text("About"),onTap: (){MyallertDialog(context);},),
+            
+
+
+
+
+          ],
+        ),
+      ),
       body:
-        Center(
-          child: ElevatedButton(onPressed: (){MyallertDialog(context);}, child: Text("Click me")),
-        )
-      );
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(padding: EdgeInsets.all(10),
+          child: TextField(
+            decoration: InputDecoration(border: OutlineInputBorder(),labelText: "First Name"),
+          ),),
+          Padding(padding: EdgeInsets.all(10),
+          child: TextField(
+            decoration: InputDecoration(border: OutlineInputBorder(),labelText: "Last Name"),
+          ),),
+          Padding(padding: EdgeInsets.all(10),
+          child: TextField(
+            decoration: InputDecoration(border: OutlineInputBorder(),labelText: "Email"),
+          ),),
+          Padding(padding: EdgeInsets.all(20),
+          child: ElevatedButton(onPressed: (){MyallertDialog(context);
+          Mysnack("Log in Succesfull", context);}, child: Text("Log IN"),style: buttonStyle,),)
+          
+        ],
 
-  }    //this is flutter practice project
+      ),
 
+
+
+    );
+  }
+  
 }
