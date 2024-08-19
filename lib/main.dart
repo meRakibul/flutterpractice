abf539584137bd60a4109d0e41e37de2e31a77b5
meRakibul <hralibul@gmail.com>
2024-08-20 03:44:56 +0600

    Working with Class

--- lib/main.dart
+++ lib/main.dart
@@ -27,72 +27,20 @@ class HomeActivity extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home"),
         backgroundColor: Colors.tealAccent,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>Scren1("This is Page1 From Home")));
-          }, child: Text("Go To 1")),
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>Scren2("This is Page2 From Home ")));
-          }, child: Text("Go To 2"))
-        ],
+    ),
+      body: Center(
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          elevation: 80,
+          color: Colors.purpleAccent,
+          shadowColor: Color.fromRGBO(33, 191, 115, 0.3),
+          child: SizedBox(
+            height: 200,
+            width: 200,
+            child: Center(child: Text("This is a card"),),
+          ),
+        ),
       ),
     );
-  }
-
-}
-
-class Scren1 extends StatelessWidget {
-  String msg;
-   Scren1(
-       this.msg,
-
-      {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: Text(msg),
-       backgroundColor: Colors.tealAccent,
-     ),
-     body: Center(
-       child: ElevatedButton(onPressed: (){
-         Navigator.push(context, MaterialPageRoute(builder: (context)=>Scren2("This is From Home")));
-       }, child: Text("Page2")),
-     ),
-   );
-  }
-
-}
-class Scren2 extends StatelessWidget {
-  String msg;
- Scren2(
-     this.msg,
-     {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-        backgroundColor: Colors.tealAccent,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeActivity()));
-          }, child: Text("Home")),
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>Scren1("THis is from Page 2")));
-          }, child: Text("Page 1"))
-        ],
-      )
-
-    );
-  }
-
+    }
 }
