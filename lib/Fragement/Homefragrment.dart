cdd24af76b991cdcfe02a7f47012a1a94b49cefe
meRakibul <hralibul@gmail.com>
2024-08-20 02:30:25 +0600

    Tapbar

---
+++ lib/Fragement/Homefragrment.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Homefragement extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Center(
+        child: Text("This is Home Friend"),
+      ),
+    );
+  }
+
+}
---
+++ lib/Fragement/SettingsFragement.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Settingsfragement extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Center(
+        child: Text("This is Search Fragement"),
+      ),
+    );
+  }
+
+}
